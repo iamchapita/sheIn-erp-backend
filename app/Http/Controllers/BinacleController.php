62ea329f796a9dd91911450c3eaf97abafc16375
iamchapita <alejandrom646@gmail.com>
2023-09-05 21:53:53 -0600

    Modelos, controladores y migraciones

---
+++ app/Http/Controllers/BinacleController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Binacle;
+use Illuminate\Http\Request;
+
+class BinacleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Binacle $binacle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Binacle $binacle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Binacle $binacle)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PaymentTrackingController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PaymentTracking;
+use Illuminate\Http\Request;
+
+class PaymentTrackingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PaymentTracking $paymentTracking)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PaymentTracking $paymentTracking)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PaymentTracking $paymentTracking)
+    {
+        //
+    }
+}
